d88fc2b39f9706ee2a6ae0000072e662b347740e
Kylee <kkeith.biz@gmail.com>
2025-10-20 14:05:55 -0400

    Reservations

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
 public class Reservation {
+    private String roomType; //King, Double, Suite
+    private int numberOfNights;
+    private double pricePerNight;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, double pricePerNight, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.pricePerNight = pricePerNight;
+        this.isWeekend = isWeekend;
+    }
+
 }
