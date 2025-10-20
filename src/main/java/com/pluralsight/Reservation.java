0e37a41ec4ce3527544a9ef2aef5fe30e05fdb86
Kylee <kkeith.biz@gmail.com>
2025-10-20 14:13:59 -0400

    Reservations setting room and pricing

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -12,5 +12,26 @@ public class Reservation {
         this.pricePerNight = pricePerNight;
         this.isWeekend = isWeekend;
     }
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType.toUpperCase();
+        if (this.roomType.equals("KING")) {
+            this.pricePerNight = 139.00;
+        }
+        else if (this.roomType.equals("DOUBLE")) {
+            this.pricePerNight = 124.00;
+        }
+        else{
+            System.out.println("Invalid Room Type");
+        }
+        if (isWeekend){
+            this.pricePerNight = this.pricePerNight * 1.10;}
+        else {
+           this.pricePerNight = this.pricePerNight;
+        }
+    }
 
 }
